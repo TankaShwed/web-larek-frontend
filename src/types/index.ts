01e57f47ef140675971de615955092b226624616
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-05-01 16:56:37 +0300

    typs

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,22 @@
-// из коллекции для постман описать типы и документация - это первая часть ревью
-
 interface IProduct {
     id: string;
+    description: string;
+    image: string;
     title: string;
+    category: string;
+    price: number;
 }
+
+type TResponseProductList = {
+    total: number;
+    items: IProduct[];
+}
+
+type TResponseProductItem = IProduct | {error: "NotFound"};
+
+interface IOrder {
+    id: string;
+    total: number;
+}
+
+type TResponseOrder = IOrder | {error: string};
