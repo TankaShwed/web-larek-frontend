a0f6e65b5a09f3bc1c8421bf323d48592e28fbf0
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-05-01 14:26:02 +0300

    добавление или удаление количества товара в корзине

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,46 @@
 import './scss/styles.scss';
 
+// добавление или удаление количества товара в корзине
+interface IBasketModel {
+	items: Map<string, number>;
+	add(id: string): void;
+	remove(id: string): void;
+}
+
+interface IEventEmiter {
+	emit: (event: string, data: unknown) => void;
+}
+
+class BasketModel implements IBasketModel {
+	constructor(protected events: IEventEmiter) {}
+
+	protected _changed() {  // метод генерирующий уведомление об изменении
+		this.events.emit('basket:change', { item: Array.from(this.items.keys()) });
+	}
+
+	add(id: string): void {
+		// ...
+		this._changed();
+	}
+	remove(id: string): void {
+		// ...
+		this._changed();
+	}
+}
+
+// class BasketModel implements IBasketModel {
+// 	items: Map<string, number> = new Map();
+
+// 	add(id: string): void {
+// 		if (!this.items.has(id)) this.items.set(id, 0); // создаем новый
+// 		this.items.set(id, this.items.get(id)! + 1); // прибавляем количество
+// 	}
+// 	remove(id: string): void {
+// 		if (!this.items.has(id)) return; //если нет, тот и делать с ним нечего
+// 		if (this.items.get(id)! > 0) {  // если есть т больше нуля то
+// 			this.items.set(id, this.items.get(id)! - 1); // уменьшить
+// 			if (this.items.get(id) === 0) this.items.delete(id); // если опустили до нуля то удалить
+// 		}
+// 	}
+// }
+
